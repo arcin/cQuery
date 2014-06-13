0f638282d9e8fa2f8b9a7a2aa3ff714e683435e6
Charles Arciniega <charlesarcin@gmail.com>
2014-06-12 20:13:02 -0700

    fixing sweet selector

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -5,26 +5,26 @@
 var SweetSelector = (function() {
   var _id = function(selector){
     if( selector.match(/^#[a-z]+/)){
-      return document.getElementById(selector.slice(1))
+      return document.getElementById(selector.slice(1));
     }
-  }
+  };
   var _class = function(selector) {
     if (selector.match(/^.[a-z]+/)) {
-      return document.getElementsByClassName(selector.slice(1))
+      return document.getElementsByClassName(selector.slice(1));
     }
-  }
+  };
 
   var _element = function(selector) {
-    var elements = document.getElementsByTagName(selector)
-    if (elements[0] == null) {
-      return false
+    var elements = document.getElementsByTagName(selector);
+    if (elements[0] === null) {
+      return false;
     }
-    return elements
-  }
+    return elements;
+  };
 
   return {
     select: function (selector) {
-       return  _id(selector) || (_element(selector) || _class(selector) )
+       return  _id(selector) || (_element(selector) || _class(selector) );
     }
-  }
-}())
+  };
+}());
